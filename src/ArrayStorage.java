4674c964d343143124c916a6aa167eb61b81aa44
Vopratka <vopratka@gmail.com>
2022-05-04 22:01:33 +0300

    Выполнено: - Заполнение массива нулевыми значениями - Копирование массива с идентификатором uuid

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,16 +1,27 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    //Заполнение массива нулевыми значениями
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+
     }
 
+    //Копирование массива с идентификатором uuid
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
@@ -20,8 +31,9 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+    //Вывод копии всех элементов массива, пока не доработан
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, storage.length);
     }
 
     int size() {
